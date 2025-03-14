ed5d7d3f7fb7d56cf60228770cae8806308449b5
hiro <hiro@DESKTOP-SRRJCEP>
2025-03-14 14:06:23 +0900

    202503141400

---
+++ samuraitravel33/src/main/java/com/example/samuraitravel33/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.samuraitravel33.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.samuraitravel33.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
